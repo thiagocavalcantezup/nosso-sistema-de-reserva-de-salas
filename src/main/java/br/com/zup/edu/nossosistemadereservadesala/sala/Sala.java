04f5e653f24cb7dcb3495e382e269cb24efd16da
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-22 18:45:39 -0300

    Use versionless opt. locking with DIRTY strategy

--- src/main/java/br/com/zup/edu/nossosistemadereservadesala/sala/Sala.java
+++ src/main/java/br/com/zup/edu/nossosistemadereservadesala/sala/Sala.java
@@ -11,10 +11,18 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.OptimisticLockType;
+import org.hibernate.annotations.OptimisticLocking;
 
 import br.com.zup.edu.nossosistemadereservadesala.exceptions.SalaNaoLivreException;
 
 @Entity
+@Table(name = "salas")
+@OptimisticLocking(type = OptimisticLockType.DIRTY)
+@DynamicUpdate
 public class Sala {
 
     @Id
